99457b08222d0a68d401325f09fe8cac7759ce39
hauretax <huggo.tricttet@hotmail.fr>
2020-10-28 19:50:00 +0100

    toto

--- ex03/main.c
+++ ex03/main.c
@@ -5,12 +5,10 @@ int main()
 	int a;
 	long int j = F_CPU / 50;
 	long int i = j;
-	a = 0;
 	for (;;)
 	{
-		DDRB |= (1 << 3); 
+		DDRB |= (1 << 3);
 		PORTB ^= (1 << 3);
-//		a += (a > 3) ? 0 : 1;
 		while (i--);
 		i = j;
 	}

984d7ec3a6af0ccc18f00963bab82f23d6a83704
pimichau <juthomas@student.42.fr>
2021-03-26 18:01:02 +0100

    temperature done bitch

--- custom_Graphic_lib/src/main.c
+++ custom_Graphic_lib/src/main.c
@@ -1,6 +1,45 @@
 #include "main.h"
 #include "screen.h"
 
+uint16_t  getTemperature() {
+
+    uint8_t tx_address = DEFAULT_I2C_ADDRESS;
+    uint8_t rx_data[TWI_BUFFER_LENGTH];
+
+    uint8_t i = 0;
+    while (i < TWI_BUFFER_LENGTH) {
+        rx_data[i++] = 0;
+    }
+
+    twi_writeTo(tx_address, 0x00, 1, true, true);
+
+    uint8_t read = twi_readFrom(tx_address, &(rx_data[0]), 2, true);
+
+    twi_writeTo(tx_address, 0, 0, true, true);
+    
+
+    return *(int16_t *)(rx_data);
+}
+
+void    print_temp(uint16_t val) {
+    uint8_t *byte = &val;
+    uint8_t temperature = (*byte & 0b01111111);
+    uint8_t is_negative = (*byte >> 7) ? 1 : 0;
+    byte++;
+    uint8_t is_point_five = (*byte >> 7) ? 1 : 0;
+    put_str("TEMPERATURE:        ", 0, 4);
+    if(is_negative) {
+        put_str("-",13,4);
+    }
+    putnbr(temperature,14,4);
+    if(is_point_five) {
+        put_str(".5", (temperature < 10) ? 15 : 16,4);
+    } else {
+        put_str(".0", (temperature < 10) ? 15 : 16,4);
+    }
+    put_str("oC", (temperature < 10) ? 17 : 18, 4);
+}
+
 unsigned int read_lux(){
     unsigned int nb;
     ADMUX &= ~(1 << MUX0) & ~(1 << MUX1) & ~(1 << MUX2) & ~(1 << MUX3);//set pin A0
@@ -54,6 +93,7 @@ extern char font[96][6];
 int       main() {
     unsigned int lux;
     unsigned int mois;
+    SREG |= 1 << SREG_I; //allows interrupt
     ADCSRA |= (1 << ADEN); // enable ADC
     ADMUX |= (1 << REFS0); // VCC -> voltage reference
    
@@ -68,7 +108,9 @@ int       main() {
     mois = read_moisture();
     put_str("HUMIDITE:    ", 0, 2);
     putnbr(mois, 12, 2);
+    print_temp(getTemperature());
     print_screen();
+
     
         // for (uint8_t i = 0; i < 113; i++)
         //     SPI_MasterTransmit(i);
